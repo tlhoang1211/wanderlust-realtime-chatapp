50879fc324f7faf4eb683e949ffb9cc1fbbf48a6
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-12-29 16:42:09 +0700

    create message item

---
+++ resources/js/components/MessageItem.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="d-flex justify-content-end mb-4" v-if="message.type === 'bot'">
+        <div class="msg_container_send bot-notification" data-toggle="tooltip" data-placement="top"
+             :title="message.created_at | toLocalTime">
+            Bot: {{ message.content }}
+        </div>
+    </div>
+    <div
+        class="msg-item d-flex justify-content-end mb-4"
+        :class="{'private': message.receiver }"
+        v-else-if="message.sender.id === $root.user.id">
+        <div class="msg-actions d-flex mr-2">
+            <div class="d-flex align-items-center">
+                <i class="fal fa-grin-alt" data-toggle="tooltip" data-placement="top" title="React"
+                   @click="showEmoji"></i>
+            </div>
+        </div>
+        <div
+            class="msg_container_send"
+            data-toggle="tooltip"
+            data-placement="top"
+            :title="message.created_at | toLocalTime"
+            :style="message.receiver ? `background-color: ${msgColor}` : ''"
+        >
+            <div v-html="highlight"></div>
+            <Reaction
+                v-if="message.reactions.length"
+                :reactions="message.reactions"
+            />
+        </div>
+        <div class="img_cont_msg" data-toggle="tooltip" data-placement="top"
+             :title="`${message.sender.name} (${message.sender.email})`">
+            <img src="/images/current_user.webp" class="rounded-circle user_img_msg">
+        </div>
+    </div>
+    <div
+        class="msg-item d-flex justify-content-start mb-4"
+        :class="{'private': message.receiver }"
+        v-else
+    >
+        <div class="img_cont_msg bg-white rounded-circle d-flex justify-content-center align-items-center"
+             data-toggle="tooltip" data-placement="top" :title="`${message.sender.name} (${message.sender.email})`">
+            <span class="rounded-circle d-flex justify-content-center align-items-center"
+                  :style="`background-color: ${message.sender.color}`">{{ message.sender.name[0].toUpperCase() }}</span>
+        </div>
+        <div class="msg_container" :class="{'bg-gray': message.receiver}" data-toggle="tooltip" data-placement="top"
+             :title="message.created_at | toLocalTime">
+            <div v-html="highlight"></div>
+            <Reaction
+                v-if="message.reactions.length"
+                :reactions="message.reactions"
+            />
+        </div>
+        <div class="msg-actions d-flex ml-2">
+            <div class="d-flex align-items-center">
+                <i class="fal fa-grin-alt" data-toggle="tooltip" data-placement="top" title="React"
+                   @click="showEmoji"></i>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import $ from 'jquery'
+import Reaction from './Reaction'
+import sanitizeHtml from 'sanitize-html' // we also need to clean data when display because old data has been cleaned
+
+export default {
+    components: {
+        Reaction
+    },
+    props: {
+        message: {
+            required: true
+        },
+        msgColor: {
+            type: String
+        }
+    },
+    mounted() {
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+    },
+    methods: {
+        showEmoji(event) {
+            this.$emit('showEmoji', this.message, event)
+        }
+    },
+    computed: {
+    }
+}
+</script>
+
