44d77e7adf77f554864e703899984d3a6f7306b8
Nam Pham <nam.pk148980@sis.hust.edu.vn>
2021-12-29 14:17:14 +0700

    create Database

--- database/migrations/2021_11_27_071106_create_messages_table.php
+++ database/migrations/2021_11_27_071106_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('room');
+            $table->bigInteger('sender')->unsigned();
+            $table->bigInteger('receiver')->unsigned()->nullable();
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('messages');
     }
 }
