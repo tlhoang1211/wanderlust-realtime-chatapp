fed6d11d47c30872e5f09d46eba8fe248b238aaf
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-12-29 15:04:18 +0700

    create Vue layout

---
+++ resources/js/App.vue
@@ -0,0 +1,28 @@
+/* eslint-disable */
+<template>
+    <div class="h-100 app-container">
+
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Footer from './components/Footer'
+
+export default {
+    components: {
+        Footer
+    },
+    data() {
+        return {
+            csrfToken: document.head.querySelector('meta[name="csrf-token"]').content,
+            transitionName: 'fade'
+        }
+    },
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
