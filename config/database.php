2d1b3fe6c0f38a5a8d57ad61c0c76d15f33108bb
Hoàng Trần <46571690+tlhoang1211@users.noreply.github.com>
2022-01-01 21:16:08 +0700

    setup laravel echo server and docker

--- config/database.php
+++ config/database.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Str;
-
 return [
 
     /*
@@ -119,11 +117,11 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+//            'prefix' => '',
         ],
 
         'default' => [
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', 'AppController@index')->middleware('auth');
+Route::get('/', 'App\Http\Controllers\AppController@index')->middleware('auth');
 
-Route::get('/messages', 'MessageController@index')->middleware('auth');
+Route::get('/messages', 'App\Http\Controllers\MessageController@index')->middleware('auth');
 
-Route::post('/messages', 'MessageController@store')->middleware('auth');
+Route::post('/messages', 'App\Http\Controllers\MessageController@store')->middleware('auth');
 
-Route::post('/reactions', 'ReactionController@react')->middleware('auth');
+Route::post('/reactions', 'App\Http\Controllers\ReactionController@react')->middleware('auth');
 
-Route::get('/{any}', 'AppController@index')->where('any', '.*')->middleware('auth'); // catch all routes or else it will return 404 with Vue router in history mode
+Route::get('/{any}', 'App\Http\Controllers\AppController@index')->where('any', '.*')->middleware('auth'); // catch all routes or else it will return 404 with Vue router in history mode
